00993a4729e7b8ccbb5671ee73934bd4614112a0
Sadia Ahmed <sadiaahmed26373@gmail.com>
2024-11-05 04:56:21 -0800

    Live Project

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -32,19 +32,21 @@ const CareersPage = () => {
         {[1, 2, 3].map((job, index) => (
           <div key={index} className="border-gray-300 ">
             <div 
-              className="bg-white flex justify-between items-center rounded-lg hover:bg-blue-950 hover:text-white shadow-sm " 
+              className={`flex justify-between items-center rounded-lg shadow-sm transition-all duration-300 
+                ${openDropdown === index ? 'bg-blue-950 text-white' : 'bg-white hover:bg-blue-950 hover:text-white'}`} 
               onClick={() => toggleDropdown(index)}  
             >
-              <span className="font-medium px-3 py-4">Job Title : Software Engineer</span>
-              <button className="text-gray-600 px-4 py-2 rounded-lg transition hover:text-white">
-                {openDropdown === index ? "▲" : "▼"}
-              </button>
+            <span className={`font-medium px-3 py-4 ${openDropdown === index ? 'text-white' : ''}`}>
+              Job Title : Software Engineer
+            </span>
+            <button className={`px-4 py-2 rounded-lg transition ${openDropdown === index ? 'text-white' : 'text-gray-600'}`}>
+            {openDropdown === index ? "▲" : "▼"}
+            </button>
             </div>
-
             {openDropdown === index && (
               <div className="mt-4 p-4 bg-gray-50 text-gray-700 rounded-lg flex flex-col items-center">
                 <h2 className="text-center font-semibold">Description:</h2>
-                <p className="text-left mt-3 mx-20">
+                <p c            lassName="text-left mt-3 mx-20">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit eos temporibus non soluta ducimus. Inventore, perferendis earum. Commodi dolore mollitia optio tempora debitis ullam alias praesentium. Esse odit sequi id, magnam cumque ab dolorem hic temporibus minus impedit, dicta suscipit tempore! Praesentium, veritatis quaerat magnam laudantium nisi obcaecati ad quasi.
                 </p>
               </div>
